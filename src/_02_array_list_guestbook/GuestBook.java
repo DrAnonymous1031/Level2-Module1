43c633f014c5d6ef0f6a2d2de75f72cfccf2dc3e
league <maddoxseul@sbsd.net>
2026-05-16 16:26:45 -0700

    h

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,7 +1,24 @@
 package _02_array_list_guestbook;
 
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
 public class GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	static JButton Polly = new JButton();
+	static JButton Gigi = new JButton();
+	ArrayList <String> yeah = new ArrayList<String>();
+	public static void main(String[] args) {
+		Polly.setText("View Names");
+		Gigi.setText("Add names");
+		Polly.addActionListener((e) -> {
+			System.out.println("bozo");
+		});
+	}
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
